927f85bf59553f9ac55ff146280d7361d1df5d6a
0xrushi <>
2025-12-18 22:27:57 -0500

    epdconfig

--- E-paper_Separate_Program/4in2_e-Paper_G/RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
+++ E-paper_Separate_Program/4in2_e-Paper_G/RaspberryPi_JetsonNano/python/lib/waveshare_epd/epdconfig.py
@@ -102,6 +102,7 @@ class RaspberryPi:
         self.SPI.writebytes(data)
 
     def spi_writebyte2(self, data):
+        print(f"spi_writebyte2: {data}")
         self.SPI.writebytes2(data)
 
     def DEV_SPI_write(self, data):
@@ -165,140 +166,218 @@ class RaspberryPi:
 
 
 
-class JetsonNano:
-    # Pin definition
-    RST_PIN  = 17
-    DC_PIN   = 25
-    CS_PIN   = 8
-    BUSY_PIN = 24
-    PWR_PIN  = 18
 
-    def __init__(self):
-        import ctypes
-        find_dirs = [
-            os.path.dirname(os.path.realpath(__file__)),
-            '/usr/local/lib',
-            '/usr/lib',
-        ]
-        self.SPI = None
-        for find_dir in find_dirs:
-            so_filename = os.path.join(find_dir, 'sysfs_software_spi.so')
-            if os.path.exists(so_filename):
-                self.SPI = ctypes.cdll.LoadLibrary(so_filename)
-                break
-        if self.SPI is None:
-            raise RuntimeError('Cannot find sysfs_software_spi.so')
+class RadxaZeroGPIOD:
+    # Absolute Linux GPIO numbers (for role matching only)
+    DC_PIN   = 560   # GPIOC_7
+    RST_PIN  = 585   # GPIOX_8
+    BUSY_PIN = 587   # GPIOX_10
+    CS_PIN   = 534   # GPIOH_6
 
-        import Jetson.GPIO
-        self.GPIO = Jetson.GPIO
+    CHIP_NAME = os.environ.get("EPD_GPIOCHIP", "gpiochip0")
 
-    def digital_write(self, pin, value):
-        self.GPIO.output(pin, value)
+    # Offsets (gpiochip0 starts at 512)
+    OFFSET_DC   = int(os.environ.get("EPD_DC_OFFSET", "48"))
+    OFFSET_RST  = int(os.environ.get("EPD_RST_OFFSET", "73"))
+    OFFSET_BUSY = int(os.environ.get("EPD_BUSY_OFFSET", "75"))
+    OFFSET_CS   = int(os.environ.get("EPD_CS_OFFSET", "22"))
 
-    def digital_read(self, pin):
-        return self.GPIO.input(self.BUSY_PIN)
+    OFFSET_MOSI = int(os.environ.get("EPD_MOSI_OFFSET", "20"))
+    OFFSET_SCLK = int(os.environ.get("EPD_SCLK_OFFSET", "23"))
 
-    def delay_ms(self, delaytime):
-        time.sleep(delaytime / 1000.0)
-
-    def spi_writebyte(self, data):
-        self.SPI.SYSFS_software_spi_transfer(data[0])
-
-    def spi_writebyte2(self, data):
-        for i in range(len(data)):
-            self.SPI.SYSFS_software_spi_transfer(data[i])
-
-    def module_init(self):
-        self.GPIO.setmode(self.GPIO.BCM)
-        self.GPIO.setwarnings(False)
-        self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
-        self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
-        self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
-        self.GPIO.setup(self.PWR_PIN, self.GPIO.OUT)
-        self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
-        
-        self.GPIO.output(self.PWR_PIN, 1)
-        
-        self.SPI.SYSFS_software_spi_begin()
-        return 0
-
-    def module_exit(self):
-        logger.debug("spi end")
-        self.SPI.SYSFS_software_spi_end()
-
-        logger.debug("close 5V, Module enters 0 power consumption ...")
-        self.GPIO.output(self.RST_PIN, 0)
-        self.GPIO.output(self.DC_PIN, 0)
-        self.GPIO.output(self.PWR_PIN, 0)
-
-        self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN, self.PWR_PIN])
-
-
-class SunriseX3:
-    # Pin definition
-    RST_PIN  = 17
-    DC_PIN   = 25
-    CS_PIN   = 8
-    BUSY_PIN = 24
-    PWR_PIN  = 18
-    Flag     = 0
+    OFFSET_PWR = os.environ.get("EPD_PWR_OFFSET")
+    PWR_ACTIVE_HIGH = os.environ.get("EPD_PWR_ACTIVE_HIGH", "1") == "1"
 
     def __init__(self):
         import spidev
-        import Hobot.GPIO
+        import gpiod
 
-        self.GPIO = Hobot.GPIO
+        self.spidev = spidev
+        self.gpiod = gpiod
         self.SPI = spidev.SpiDev()
 
+        self._chip = None
+        self._line_dc = None
+        self._line_rst = None
+        self._line_busy = None
+        self._line_cs = None
+        self._line_mosi = None
+        self._line_sclk = None
+        self._line_pwr = None
+
+        self._use_spidev = (
+            os.environ.get("EPD_USE_SPIDEV", "1") == "1"
+            and os.environ.get("EPD_BITBANG", "0") != "1"
+        )
+
+        self._use_gpio_cs = os.environ.get("EPD_USE_GPIO_CS", "1") == "1"
+        self._bb_delay_us = int(os.environ.get("EPD_BB_DELAY_US", "3"))
+        self._busy_invert = os.environ.get("EPD_BUSY_INVERT", "0") == "1"
+        self._dump_file = open("dump.txt", "w")
+
+    # ------------------------------------------------------------------ GPIO
+
     def digital_write(self, pin, value):
-        self.GPIO.output(pin, value)
+        v = 1 if value else 0
+        if pin == self.DC_PIN and self._line_dc:
+            self._line_dc.set_value(v)
+        elif pin == self.RST_PIN and self._line_rst:
+            self._line_rst.set_value(v)
+        elif pin == self.CS_PIN and self._line_cs:
+            self._line_cs.set_value(v)
 
     def digital_read(self, pin):
-        return self.GPIO.input(pin)
+        if pin == self.BUSY_PIN and self._line_busy:
+            val = self._line_busy.get_value()
+            return val ^ (1 if self._busy_invert else 0)
+        return 0
 
-    def delay_ms(self, delaytime):
-        time.sleep(delaytime / 1000.0)
+    def delay_ms(self, ms):
+        time.sleep(ms / 1000.0)
+
+    # ------------------------------------------------------------------ SPI
 
     def spi_writebyte(self, data):
-        self.SPI.writebytes(data)
+        if self._use_spidev:
+            data = list(data)
+            self._dump_file.write(f"SPI Write: {[f'0x{b:02X}' for b in data]}\n")
+            self._dump_file.flush()
+            self.SPI.writebytes(data)
+        else:
+            self._spi_bitbang(data)
 
     def spi_writebyte2(self, data):
-        # for i in range(len(data)):
-        #     self.SPI.writebytes([data[i]])
-        self.SPI.xfer3(data)
+        if self._use_spidev:
+            # Use writebytes2 for bulk transfers (proven to work on Radxa Zero)
+            self._dump_file.write(f"SPI Write2 ({len(data)} bytes): {[f'0x{b:02X}' for b in data]}\n")
+            self._dump_file.flush()
+            
+            if isinstance(data, (bytes, bytearray)):
+                self.SPI.xfer3(list(data), delay_usec=1200)
+            else:
+                self.SPI.xfer3(data, delay_usec=1200)
+        else:
+            self._spi_bitbang(data)
+
+    def _spi_bitbang(self, data):
+        half = self._bb_delay_us / 1_000_000.0
+        seq = data if isinstance(data, (bytes, bytearray)) else list(data)
+
+        for b in seq:
+            if self._line_cs:
+                self._line_cs.set_value(0)
+
+            for i in range(7, -1, -1):
+                self._line_sclk.set_value(0)
+                self._line_mosi.set_value((b >> i) & 1)
+                time.sleep(half)
+                self._line_sclk.set_value(1)
+                time.sleep(half)
+
+            if self._line_cs:
+                self._line_cs.set_value(1)
+
+    # ------------------------------------------------------------------ INIT
+
+    def _request_out(self, line, name, default):
+        try:
+            line.request(
+                consumer=name,
+                type=self.gpiod.LINE_REQ_DIR_OUT,
+                default_val=default,
+            )
+        except TypeError:
+            line.request(
+                consumer=name,
+                type=self.gpiod.LINE_REQ_DIR_OUT,
+            )
+            line.set_value(default)
 
     def module_init(self):
-        if self.Flag == 0:
-            self.Flag = 1
-            self.GPIO.setmode(self.GPIO.BCM)
-            self.GPIO.setwarnings(False)
-            self.GPIO.setup(self.RST_PIN, self.GPIO.OUT)
-            self.GPIO.setup(self.DC_PIN, self.GPIO.OUT)
-            self.GPIO.setup(self.CS_PIN, self.GPIO.OUT)
-            self.GPIO.setup(self.PWR_PIN, self.GPIO.OUT)
-            self.GPIO.setup(self.BUSY_PIN, self.GPIO.IN)
+        self._chip = self.gpiod.Chip(self.CHIP_NAME)
 
-            self.GPIO.output(self.PWR_PIN, 1)
-        
-            # SPI device, bus = 0, device = 0
-            self.SPI.open(2, 0)
-            self.SPI.max_speed_hz = 4000000
-            self.SPI.mode = 0b00
-            return 0
-        else:
-            return 0
+        self._line_dc = self._chip.get_line(self.OFFSET_DC)
+        self._line_rst = self._chip.get_line(self.OFFSET_RST)
+        self._line_busy = self._chip.get_line(self.OFFSET_BUSY)
+        self._line_cs = self._chip.get_line(self.OFFSET_CS)
 
-    def module_exit(self):
-        logger.debug("spi end")
-        self.SPI.close()
+        if not self._use_spidev:
+            self._line_mosi = self._chip.get_line(self.OFFSET_MOSI)
+            self._line_sclk = self._chip.get_line(self.OFFSET_SCLK)
 
-        logger.debug("close 5V, Module enters 0 power consumption ...")
-        self.Flag = 0
-        self.GPIO.output(self.RST_PIN, 0)
-        self.GPIO.output(self.DC_PIN, 0)
-        self.GPIO.output(self.PWR_PIN, 0)
+        if self.OFFSET_PWR is not None:
+            self._line_pwr = self._chip.get_line(int(self.OFFSET_PWR))
 
-        self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN], self.PWR_PIN)
+        self._request_out(self._line_dc, "epd-dc", 0)
+        self._request_out(self._line_rst, "epd-rst", 1)
+
+        self._line_busy.request(
+            consumer="epd-busy",
+            type=self.gpiod.LINE_REQ_DIR_IN,
+        )
+
+        self._request_out(self._line_cs, "epd-cs", 1)
+
+        if not self._use_spidev:
+            self._request_out(self._line_mosi, "epd-mosi", 0)
+            self._request_out(self._line_sclk, "epd-sclk", 0)
+
+        if self._line_pwr:
+            self._request_out(
+                self._line_pwr,
+                "epd-pwr",
+                1 if self.PWR_ACTIVE_HIGH else 0,
+            )
+
+        if self._use_spidev:
+            bus = int(os.environ.get("EPD_SPI_BUS", "1"))
+            dev = int(os.environ.get("EPD_SPI_DEV", "0"))
+            self.SPI.open(bus, dev)
+
+            if self._use_gpio_cs:
+                try:
+                    self.SPI.no_cs = True
+                except OSError as e:
+                    print(f"WARNING: Failed to set SPI.no_cs=True: {e}")
+
+            self.SPI.max_speed_hz = int(
+                os.environ.get("EPD_SPI_HZ", "2000000")
+            )
+            self.SPI.mode = 0
+
+        for attr in dir(self):
+            print("self.%s = %r" % (attr, getattr(self, attr)))
+        return 0
+
+    # ------------------------------------------------------------------ EXIT
+
+    def module_exit(self, cleanup=True):
+        if self._use_spidev:
+            try:
+                self.SPI.close()
+            except Exception:
+                pass
+
+        for line in (
+            self._line_dc,
+            self._line_rst,
+            self._line_cs,
+            self._line_mosi,
+            self._line_sclk,
+            self._line_busy,
+            self._line_pwr,
+        ):
+            if line:
+                try:
+                    line.release()
+                except Exception:
+                    pass
+
+        try:
+            if self._chip:
+                self._chip.close()
+        except Exception:
+            pass
 
 
 if sys.version_info[0] == 2:
@@ -309,12 +388,7 @@ output, _ = process.communicate()
 if sys.version_info[0] == 2:
     output = output.decode(sys.stdout.encoding)
 
-if "Raspberry" in output:
-    implementation = RaspberryPi()
-elif os.path.exists('/sys/bus/platform/drivers/gpio-x3'):
-    implementation = SunriseX3()
-else:
-    implementation = JetsonNano()
+implementation = RadxaZeroGPIOD()
 
 for func in [x for x in dir(implementation) if not x.startswith('_')]:
     setattr(sys.modules[__name__], func, getattr(implementation, func))
